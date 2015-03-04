60f43dd12b3253363b8f52965213301ec714f120
dbarobin <dbarobinwen@gmail.com>
2015-03-04 17:17:49 +0800

    [Scripts] Update scripts. - Update commit info

--- add_article.sh
+++ add_article.sh
@@ -2,7 +2,8 @@
 # Author: Robin Wen
 # Date: 2015-1-10 15:47:08
 # Desc: Auto push after add article.
+# Update: Update Commit info at 2015-03-04 17:16:28
 
 git add -A .
-git commit -m "add article"
+git commit -m "[Post] Add article."
 git push origin master
--- update_article.sh
+++ update_article.sh
@@ -2,7 +2,8 @@
 # Author: Robin Wen
 # Date: 2015-1-1 08:49:08
 # Desc: Auto push after update the repo.
+# Update: Update Commit info at 2015-03-04 17:17:10
 
 git add -A .
-git commit -m "update article"
+git commit -m "[Post] Update article."
 git push origin master
